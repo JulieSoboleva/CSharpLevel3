402e67f6a33badd72d38d6e8ad9112b9fe1f39c4
JulieSoboleva <ju.soboll@gmail.com>
2020-05-15 17:11:50 +0300

    Home work 6

---
+++ Matrix/Matrix/Program.cs
@@ -0,0 +1,51 @@
+﻿/* Даны две двумерных матрицы размерностью 100х100 каждая. 
+ * Задача: написать приложение, производящее их параллельное умножение. 
+ * Матрицы заполняются случайными целыми числами от 0 до 10.
+*/
+
+using System;
+using System.Threading.Tasks;
+using Array2DLib;
+
+namespace Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Array2D m1 = new Array2D(100, 100, 0, 10); // ("..//..//..//..//matrix1.txt");
+            Console.WriteLine("Матрица А: ");
+            m1.Print();
+
+            Array2D m2 = new Array2D(100, 100, 0, 10); // ("..//..//..//..//matrix2.txt");
+            Console.WriteLine("Матрица B: ");
+            m2.Print();
+
+            Console.WriteLine("Запуск умножения матриц.");
+            Array2D m3 = new Array2D(m1.RowCount, m2.ColumnCount);
+            Task<int>[] tasks = new Task<int>[m1.RowCount * m2.ColumnCount];
+            for (int t = 0, i = 0; i < m1.RowCount; i++)
+            {
+                for (int j = 0; j < m2.ColumnCount; j++, t++)
+                {
+                    tasks[t] = Task.Factory.StartNew(() => TaskMethodMulti(m1.GetRow(i), m2.GetColumn(j)));
+                    m3.SetCell(i, j, tasks[t].Result);
+                }
+            }
+            Task.WaitAll();
+            Console.WriteLine("Матрица C: ");
+            m3.Print();
+            m3.ArrayToFile("..//..//..//..//matrix3.txt");
+            Console.WriteLine("Перемножение матриц выполнено!");
+            Console.ReadKey();
+        }
+
+        static int TaskMethodMulti(int[] row, int[] column)
+        {
+            int sum = 0;
+            for (int i = 0; i < row.Length; i++)
+                sum += row[i] * column[i];
+            return sum;
+        }
+    }
+}
---
+++ WpfWorkWithFiles/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿/*	* В директории лежат файлы.
+По структуре они содержат три числа, разделенные пробелами.
+Первое число — целое, обозначает действие: 1 — умножение и 2 — деление.
+Остальные два — числа с плавающей точкой.
+Задача: написать многопоточное приложение, 
+выполняющее эти действия над числами и сохраняющее результат в файл result.dat.
+Файлов в директории заведомо много. */
+
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfWorkWithFiles
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow() => InitializeComponent();
+
+        static Task<string> CreateFilesAsync(int Count)
+        {
+            return Task.Run(() =>
+            {
+                try
+                {
+                    string path = "..//..//..//Files";
+                    DirectoryInfo dirWorkInfo = Directory.CreateDirectory(path);
+                    Random rnd = new Random();
+                    for (int i = 0; i < Count; i++)
+                        File.WriteAllText(path + "//" + (i + 1).ToString() + ".dat", rnd.Next(1, 3).ToString() + " " + rnd.NextDouble() * 100 + " " + rnd.NextDouble() * 10);
+                    return "Файлы созданы.";
+                }
+                catch (Exception exn)
+                {
+                    return exn.Message;
+                }
+            });
+        }
+
+        private async void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+            string[] files = Directory.GetFiles("..//..//..//Files");
+            pbTemp.Minimum = 0;
+            pbTemp.Maximum = files.Length;
+            Task<double>[] tasks = new Task<double>[files.Length];
+            for (int i=0; i < tasks.Length; i++)
+            {
+                pbTemp.Value = i;
+                string[] str = File.ReadAllText(files[i]).Split(' ');
+                tasks[i] = MethodAsync(int.Parse(str[0]), double.Parse(str[1]), double.Parse(str[2]));
+            }
+
+            await Task.WhenAll(tasks);
+            pbTemp.Value = 0;
+
+            for (int i=0; i < tasks.Length; i++)
+                files[i] = tasks[i].Result.ToString();
+
+            File.WriteAllLines("..//..//..//result.dat", files);
+            lStatus.Content = "Работа с файлами завершена.";
+        }
+
+        static Task<double> MethodAsync(int operation, double a, double b)
+        {
+            return (operation == 1) ? Task.Run(() => { return a * b; }) : Task.Run(() => { return a / b; });
+        }
+
+        private async void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            lStatus.Content = await CreateFilesAsync(20000);
+            btnStart.IsEnabled = true;
+        }
+    }
+}
